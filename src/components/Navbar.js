92b66d0981a2437e120b831f0fb4fa1c43ed9bb7
MBUDA-BRANICE-NJIRA <mbudanjira@gmail.com>
2025-03-10 23:56:52 +0300

    Styled the navigation bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,7 @@ import { AiFillCaretDown } from "react-icons/ai";
 function Navbarcompo() {
   return (
     <div className="navbar">
+      
       <h1>Student Api</h1>
 
       <div className="links">
