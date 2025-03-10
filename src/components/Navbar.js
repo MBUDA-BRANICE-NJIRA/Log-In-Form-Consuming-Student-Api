a2a91ca98a28fe2df98257b305136a2fa89b1e43
MBUDA-BRANICE-NJIRA <mbudanjira@gmail.com>
2025-03-10 22:45:19 +0300

    Added a dropdown section for students

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import React from 'react';
+import React from "react";
 import { AiFillCaretDown } from "react-icons/ai";
 
 function Navbarcompo() {
@@ -7,12 +7,29 @@ function Navbarcompo() {
     <div className="navbar">
       <h1>Student Api</h1>
       <div className="links">
-        <Link to="/AddStudent" className="nav_links">Student<AiFillCaretDown className="icon"/></Link>
-        <Link to="/LogInForm" className="nav_links">LogIn</Link>
-        <Link to="/LogInForm" className="nav_links">LogOut</Link>
+        <Link to="/AddStudent" className="nav_links">
+          Student
+          <AiFillCaretDown className="icon" />
+        </Link>
+
+        <div className="dropdown-menu">
+          <Link to="/All-Students" className="dropdown-link">
+            All Students
+          </Link>
+          <Link to="/Add-Student" className="dropdown-link">
+            Add Student
+          </Link>
+        </div>
+
+        <Link to="/LogInForm" className="nav_links">
+          LogIn
+        </Link>
+        <Link to="/LogInForm" className="nav_links">
+          LogOut
+        </Link>
       </div>
     </div>
   );
-};
+}
 
 export default Navbarcompo;
