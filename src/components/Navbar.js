07a0527da78b41e48a08259deb689a58d1c0a2a7
MBUDA-BRANICE-NJIRA <mbudanjira@gmail.com>
2025-03-10 22:25:23 +0300

    Worked on the designing of dispay partern

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom";
 import React from 'react';
-import { FaChevronDown } from "react-icons/fa";
+import { AiFillCaretDown } from "react-icons/ai";
 
 function Navbarcompo() {
   return (
     <div className="navbar">
       <h1>Student Api</h1>
       <div className="links">
-        <Link to="/AddStudent" className="nav_links">Student<FaChevronDown /></Link>
+        <Link to="/AddStudent" className="nav_links">Student<AiFillCaretDown className="icon"/></Link>
         <Link to="/LogInForm" className="nav_links">LogIn</Link>
+        <Link to="/LogInForm" className="nav_links">LogOut</Link>
       </div>
     </div>
   );
