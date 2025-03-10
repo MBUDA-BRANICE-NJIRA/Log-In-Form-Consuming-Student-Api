4247da82837a0466fa7d4937e19ba142fc6d7163
MBUDA-BRANICE-NJIRA <mbudanjira@gmail.com>
2025-03-10 21:52:48 +0300

    Imported the react icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
 import React from 'react';
+import { FaChevronDown } from "react-icons/fa";
 
 function Navbarcompo() {
   return (
     <div className="navbar">
       <h1>Student Api</h1>
       <div className="links">
-        <Link to="/AddStudent" className="nav_links">Student</Link>
+        <Link to="/AddStudent" className="nav_links">Student<FaChevronDown /></Link>
         <Link to="/LogInForm" className="nav_links">LogIn</Link>
       </div>
     </div>
