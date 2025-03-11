7ad5f207b4600d34f62bc8d7648beeb1eb5c9007
MBUDA-BRANICE-NJIRA <mbudanjira@gmail.com>
2025-03-11 20:27:06 +0300

    Succesfuly made the table for all students

--- src/components/AllStudentTable.js
+++ src/components/AllStudentTable.js
@@ -1,40 +1,38 @@
-import React from "react";
-
-
-
 function Tablecompo() {
-  const records = {
-    body: (callback) => [
-    
-    ].map(callback)
-  };
+    const records = {
+      body: (callback) => [
+        // your data here
+      ].map(callback)
+    };
   
-  return (
-    <div className="container">
-        <div className="AllStudents">
-            <h1>All Students Details</h1>
-            <table className="table-border">
-                <thead>
-                    <tr>Firstname</tr>
-                    <tr>Lastname</tr>
-                    <tr>Gender</tr>
-                    <tr>Action</tr>
-                </thead>
-
-                <tbody>
-                    {records.body((r, i) =>(
-                        <tr key={i}>
-                            <td>{r.firstname}</td>
-                            <td>{r.lastname}</td>
-                            <td>{r.gender}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    
-    </div>
-  );
-}
-
-export default Tablecompo;
+    return (
+      <div className="container">
+        <h1>All Students Details</h1>
+        <table className="table-border">
+          <thead>
+            <tr>
+              <th>Firstname</th>
+              <th>Lastname</th>
+              <th>Gender</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+  
+          <tbody>
+            {records.body((r, i) => (
+              <tr key={i}>
+                <td>{r.firstname}</td>
+                <td>{r.lastname}</td>
+                <td>{r.gender}</td>
+                <td className="action-column">
+                  {/* your action buttons here */}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+  
+  export default Tablecompo;
